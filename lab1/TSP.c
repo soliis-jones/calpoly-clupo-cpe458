22d5c9f83a9f0562654b96ddfa012c77c6018739
travesteez <travisemichael@gmail.com>
2016-04-06 16:13:34 -0700

    Created TSP.c. Wrote TSP.c to be a simple MPI program to test using MPI.

---
+++ lab1/TSP.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <mpi.h>
+
+int main(int argc, char **argv) {
+   double best = -1.0;
+    int my_id, num_procs;
+
+   MPI_Init(&argc, &argv);
+
+   MPI_Comm_rank(MPI_COMM_WORLD, &my_id);
+   MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
+
+   printf("I am %d out of %d!\n", my_id, num_procs);
+
+   MPI_Finalize();
+
+   return 0;
+}
