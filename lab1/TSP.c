25f612db5f0f1e81ca0e5963f716578b8323b74c
travesteez <travisemichael@gmail.com>
2016-04-08 16:04:57 -0700

    started actually working on the lab

--- lab1/TSP.c
+++ lab1/TSP.c
@@ -5,14 +5,28 @@
 
 int main(int argc, char **argv) {
    //double best = -1.0;
-    int my_id, num_procs;
+   int my_id, num_procs, num_cities, num_iter, i, j;
+   int *paths;
+
+   num_cities = atoi(argv[1]);
+   path = malloc(atoi(num_cities*sizeof(int));
+   num_iter = atoi(argv[2]);
+
+   for (i = 0; i < num_cities - 1; i++) {
+      path[i] = i + 1;
+   }
+   path[i] = 0;
 
    MPI_Init(&argc, &argv);
-
+   
    MPI_Comm_rank(MPI_COMM_WORLD, &my_id);
    MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
+   
+   i = rand() % num_cities;
+   j = rand() % num_cities;
 
-   printf("I am %d out of %d!\n", my_id, num_procs);
+   paths[i] = j;
+   paths[i+1] = j+1;
 
    MPI_Finalize();
 
